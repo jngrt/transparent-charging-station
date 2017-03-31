793d9613d5ff62b7c7a2827f653c40a75e23954e
Marcel Schouwenaar <marcel@the-incredible-machine.com>
2017-03-31 10:57:37 +0200

    Simplified play algorithm.

--- app/app.js
+++ app/app.js
@@ -26,7 +26,7 @@ const _ = require('underscore')
 const jQuery = require('jquery')
 const $ = jQuery;
 
-const [ABSOLUTE_PRIORITY, MEDIUM_PRIORITY, NO_PRIORITY, GREEN_PRIORITY] = [1, 2, 3, 4];
+// const [ABSOLUTE_PRIORITY, MEDIUM_PRIORITY, NO_PRIORITY, GREEN_PRIORITY] = [1, 2, 3, 4];
 
 const greenThreshold = 6; //6 gray energy, 6 green energy
 
@@ -35,32 +35,26 @@ const greenThreshold = 6; //6 gray energy, 6 green energy
 jQuery(document).ready(function ($) {
 
 	const tetris = new Tetris();
-	//tetris.init();
-	dummyData(tetris);
 
 	swarm = new Swarm();
 	tetris.onUpdate(() => swarm.update( tetris ));
 
 
 	$('form.addClaims :input').on('input', function (evt) {
+		var form = $(evt.target).parents("form");
+		getDataFromForm(form, tetris);
 		evt.preventDefault();
-
-		let data = $(evt.target).parents("form").serializeArray().reduce(function (obj, item) {
-			obj[item.name] = item.value;
-			return obj;
-		}, {});
-
-		console.log("data",data);
-
-		tetris.addClaim(+data.claimer, +data.priority, +data.chargeNeeded, +data.deadline);
-		return false;
 	})
+	$("form").each(function(index){
+		getDataFromForm($(this), tetris);
+	});
 });
 
-function dummyData(t) {
-	//claimer, priority, chargeNeeded, deadline, info){
-	t.addClaim(1, 10, 60, 10);
-	t.addClaim(2, 10, 60, 20);
-	t.addClaim(3, 10, 200, 30);
+function getDataFromForm(form, tetris){
+	let data = form.serializeArray().reduce(function (obj, item) {
+		obj[item.name] = item.value;
+		return obj;
+	}, {});
 
+	tetris.addClaim(+data.claimer, +data.priority, +data.chargeNeeded, +data.deadline);
 }
