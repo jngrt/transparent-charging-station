9b1843d7a15ca1fa6d431148c3236a74db19aa38
Marcel Schouwenaar <marcel@the-incredible-machine.com>
2017-04-07 18:32:11 +0200

    rolled back replay check

--- app/app.js
+++ app/app.js
@@ -67,7 +67,7 @@ jQuery(document).ready(function ($) {
 	swarm = new NewSwarm("#tetris_ui");
 	tetris.onUpdate(() => {
 		
-		if(appState == REPLAY) return;
+		// if(appState == REPLAY) return;
 		
 		swarm.update(tetris.getCurrentGrid());
 
