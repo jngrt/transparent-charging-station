f24b358ed13c318b491a3af7fc07e0e792742393
jngrt <jangeertmunneke@gmail.com>
2017-04-10 10:12:03 +0200

    remove auto-launch

--- main.js
+++ main.js
@@ -11,18 +11,6 @@ const jQuery = require('jquery')
 
 require('electron-reload')(__dirname);
 
-let AutoLaunch = require('auto-launch');
- 
-let appLauncher = new AutoLaunch({
-    name: 'My NW.js or Electron app'
-});
- 
-appLauncher.isEnabled().then(function(enabled){
-    if(enabled) return;
-    return appLauncher.enable()
-}).then(function(err){
-    console.log('auto-launch error: ', err);
-});
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
