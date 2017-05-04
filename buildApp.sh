fe0407da3ac545de306a5855e65cd841250421ce
Admin <admin@ChargingStation.local>
2017-05-04 13:27:43 +0200

    shell scripts

--- buildApp.sh
+++ buildApp.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
+git pull
 echo 'Building new version...'
 NOW=$(date +"%m-%d-%Y_%H-%M-%S")
 mkdir ./builds/$NOW
-electron-packager ./ChargingStationCode ChargingStation --overwrite --platform=darwin --arch=x64 --out ./builds/$NOW
+electron-packager ./ ChargingStation --overwrite --platform=darwin --arch=x64 --out ./builds/$NOW
 CURDIR=$(pwd)
+rm /Applications/ChargingStation.app
 ln -s "$CURDIR/builds/$NOW/ChargingStation-darwin-x64/ChargingStation.app" /Applications/ChargingStation.app
